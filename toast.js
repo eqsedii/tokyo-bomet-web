df9d9e0ab6a5ffe4c91026523764e2533cb56095
eqsedii <arphaxad98@gmail.com>
2026-08-28 10:06:27 +0300

    Add showToast function for displaying messages

---
+++ toast.js
@@ -0,0 +1,18 @@
+export function showToast(message, duration = 2200) {
+  let container = document.getElementById('toastContainer');
+  if (!container) {
+    container = document.createElement('div');
+    container.id = 'toastContainer';
+    container.className = 'toast-container';
+    document.body.appendChild(container);
+  }
+  const toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.innerText = message;
+  container.appendChild(toast);
+  requestAnimationFrame(() => toast.classList.add('show'));
+  setTimeout(() => {
+    toast.classList.remove('show');
+    setTimeout(() => toast.remove(), 300);
+  }, duration);
+}
